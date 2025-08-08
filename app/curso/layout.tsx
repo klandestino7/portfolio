f759ffff67c156b0d9f4419adcb9fdb1ecb7207f
klandestino7 <klandestino6@hotmail.com>
2025-08-08 16:48:33 -0300

    chore: add thumbnail and metadata

--- app/curso/layout.tsx
+++ app/curso/layout.tsx
@@ -1,8 +1,24 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-    title: 'Curso Dominando o FIVEM/REDM',
+    title: 'Curso Dominando o FiveM/RedM',
     description: 'Criado por klandestino, prenda a criar scripts profissionais de FiveM e RedM para vender no mercado internacional ou abrir seu próprio servidor lucrativo. Desenvolvedores FiveM ganham entre $500 - $3000 USD/mês trabalhando de casa.',
+    openGraph: {
+        title: 'Curso Dominando o FiveM/RedM',
+        description: 'Criado por klandestino, prenda a criar scripts profissionais de FiveM e RedM para vender no mercado internacional ou abrir seu próprio servidor lucrativo. Desenvolvedores FiveM ganham entre $500 - $3000 USD/mês trabalhando de casa.',
+        url: "https://curso.klandestino.dev",
+        siteName: "Klandestino",
+        images: [
+            {
+                url: "https://klandestino.dev/thumbnail.jpg", // caminho da sua thumbnail
+                width: 1200,
+                height: 630,
+                alt: "Thumbnail do Curso",
+            },
+        ],
+        locale: "pt_BR",
+        type: "website",
+    },
 }
 
 export default function RootLayout({
